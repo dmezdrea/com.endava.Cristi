dd4677f51cd349cbb4ed463de5cc49e4baea9f6a
dmezdrea <dumitru.mezdrea@endava.com>
2015-08-04 09:59:29 +0300

    Update tema
    final update

---
+++ src/test/java/com/endava/tema_cristi/MainClass.java
@@ -0,0 +1,14 @@
+package com.endava.tema_cristi;
+
+import org.junit.Test;
+
+/**
+ * Created by dmezdrea on 8/3/2015.
+ */
+public class MainClass {
+
+    @Test
+    public void main(){
+        System.out.println("Hello Cristi");
+    }
+}
